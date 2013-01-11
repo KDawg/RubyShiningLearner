b13b2f68f61d6b5ee7e85cebef7a3c8bde54129b
Ken Tabor <katworks@gmail.com>
2013-01-11 17:37:38 -0600

    more experimenting

--- classes/color.rb
+++ classes/color.rb
@@ -3,6 +3,12 @@
 
 class Color
 
+  attr_reader :name, :count
+
+  def name=(new_name)
+    @name = new_name
+  end
+
   def initialize(name, count = 1)
     # @ symbol makes this a class instance variable
     @name = name
@@ -10,28 +16,34 @@ class Color
   end
 
   def say_name
-    print "my name is[#{@name}]"
+    print "my name is [#{@name}]" if @name != nil
   end
 
   def show_values
     values = {
-      count => @count,
-      name => @name,
-      customer => 'Harrison Ford'
+      'count' => @count,
+      'name' => @name,
+      'customer' => 'Harrison Ford'
     }
     print values
   end
 
+  public :name, :initialize, :say_name, :show_values
+
 end
 
 
-color_options = ['red', 'green', 'blue']
+color_options = ['red', 'green', 'blue', 42.1234]
 color_options.each do |color|
   puts "color option[#{color}]"
 end
 
 the_color = Color.new("pale green")
 the_color.say_name
+puts the_color.count
+puts the_color.name
+the_color.name = "Scarlett Johanson"
+puts the_color.name
 
 if not ARGV.empty?
   puts ARGV
